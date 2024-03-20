e0cb18108aeb376eb27f0cf27ce5573b552ee68b
Ioseba Palop <4988743+iosebyte@users.noreply.github.com>
2024-03-20 10:58:42 +0100

    Add .net Standard support

--- LatchResponse.cs
+++ LatchResponse.cs
@@ -1,39 +1,67 @@
 ﻿/*
-    Returns the Latch responses according to the configuration set.
-    Copyright (C) 2023 Telefonica Digital
- 
-    This library is free software; you can redistribute it and/or
-    modify it under the terms of the GNU Lesser General Public
-    License as published by the Free Software Foundation; either
-    version 2.1 of the License, or (at your option) any later version.
- 
-    This library is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-    Lesser General Public License for more details.
- 
-    You should have received a copy of the GNU Lesser General Public
-    License along with this library; if not, write to the Free Software
-    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+   Returns the Latch responses according to the configuration set.
+   Copyright (C) 2023 Telefonica Digital
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with this library; if not, write to the Free Software
+   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
 */
 
 using System;
 using System.Collections.Generic;
-using System.Text;
+#if NETSTANDARD
+using System.Text.Json;
+using System.Text.Json.Nodes;
+#endif
+#if NETFRAMEWORK
 using System.Web.Script.Serialization;
-
+#endif
 namespace LatchSDK
 {
     public class LatchResponse
     {
+#if NETSTANDARD
+        public JsonObject Data { get; private set; }
+#endif
+
+#if NETFRAMEWORK
+        private static JavaScriptSerializer js = new JavaScriptSerializer();
         public Dictionary<string, object> Data { get; private set; }
+#endif
         public Error Error { get; private set; }
 
-        private static JavaScriptSerializer js = new JavaScriptSerializer();
-
-        public LatchResponse(String json)
+        public LatchResponse(string json)
         {
+#if NETSTANDARD
+            var response = JsonSerializer.Deserialize<JsonObject>(json);
+            if (response.TryGetPropertyValue("data", out var dataValue))
+            {
+                this.Data = dataValue.AsObject();
+            }
+
+            if (response.TryGetPropertyValue("error", out var err))
+            {
+                var error = err.AsObject();
+                if (error.TryGetPropertyValue("code", out var codeNode))
+                {
+                    String message = error.TryGetPropertyValue("message", out var messNode) ? messNode.GetValue<string>() : String.Empty;
+                    this.Error = new Error(codeNode.GetValue<int>(), message);
+                }
+            }
+#endif
+#if NETFRAMEWORK
+
             Dictionary<string, object> response = (Dictionary<string, object>)js.DeserializeObject(json);
             if (response.ContainsKey("data"))
             {
@@ -50,6 +78,7 @@ namespace LatchSDK
                     this.Error = new Error(code, message);
                 }
             }
+#endif
         }
     }
 }
